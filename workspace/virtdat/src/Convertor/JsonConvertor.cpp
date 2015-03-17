b686056e673ddd8d6b7e562cb9ee867b2e26e227
0x000FF4 <ddd32@abv.bg>
2015-03-17 23:42:01 +0200

    started json export

---
+++ workspace/virtdat/src/Convertor/JsonConvertor.cpp
@@ -0,0 +1,21 @@
+/*
+ * JsonConvertor.cpp
+ *
+ *  Created on: 17.03.2015 г.
+ *      Author: x000ff4
+ */
+
+#include "JsonConvertor.h"
+
+namespace std {
+
+JsonConvertor::JsonConvertor() {
+	// TODO Auto-generated constructor stub
+
+}
+
+JsonConvertor::~JsonConvertor() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
---
+++ workspace/virtdat/src/Convertor/JsonConvertor.h
@@ -0,0 +1,21 @@
+/*
+ * JsonConvertor.h
+ *
+ *  Created on: 17.03.2015 г.
+ *      Author: x000ff4
+ */
+
+#ifndef CONVERTOR_JSONCONVERTOR_H_
+#define CONVERTOR_JSONCONVERTOR_H_
+
+namespace std {
+
+class JsonConvertor {
+public:
+	JsonConvertor();
+	virtual ~JsonConvertor();
+};
+
+} /* namespace std */
+
+#endif /* CONVERTOR_JSONCONVERTOR_H_ */
