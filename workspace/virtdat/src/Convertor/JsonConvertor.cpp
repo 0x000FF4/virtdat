acd7d80a161c853e01f2db60f944d2cc2ba13bb1
0x000FF4 <ddd32@abv.bg>
2015-03-18 23:41:50 +0200

    json almost done

--- workspace/virtdat/src/Convertor/JsonConvertor.cpp
+++ workspace/virtdat/src/Convertor/JsonConvertor.cpp
@@ -7,7 +7,29 @@
 
 #include "JsonConvertor.h"
 
+#include <string>
+
+#include "../VirtualTable/VirtColum.h"
+
 namespace std {
+
+string JsonConvertor::generateJson(vector<VirtColum*> cloumns,vector<vector<string>> rows){
+	rapidjson::Document d;
+	rapidjson::Value v;
+	v.SetObject();
+	rapidjson::Document::AllocatorType& allocator = d.GetAllocator();
+	v.AddMember("playername","2", allocator);
+	//d.AddMember("",rapidjson::Value(rows.at(0).at(0)));
+
+
+    // 3. Stringify the DOM
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    d.Accept(writer);
+
+    // Output {"project":"rapidjson","stars":11}
+    return buffer.GetString();
+}
 
 JsonConvertor::JsonConvertor() {
 	// TODO Auto-generated constructor stub
