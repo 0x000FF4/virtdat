67a040b38ba8fef9e8f301f12e3af1c0e9217a58
0x000FF4 <ddd32@abv.bg>
2015-03-20 22:49:08 +0200

    no work

--- workspace/virtdat/src/Convertor/JsonConvertor.cpp
+++ workspace/virtdat/src/Convertor/JsonConvertor.cpp
@@ -16,20 +16,24 @@ namespace std {
 string JsonConvertor::generateJson(vector<VirtColum*> cloumns,vector<vector<string>> rows){
 	rapidjson::Document d;
 	vector<rapidjson::Value> columns;
+
 	for(int i = 0 ; i < columns.size();i++){
-		rapidjson::Value v ;
-			v.SetArray();
+		    rapidjson::Value v ;
+			v.SetInt();//utre 6te go naprawq po dobre
+
 			columns.push_back(v);
 	}
 	for(int i = 0 ; i< rows.size();i++){
 		for(int j = 0 ; i < rows.at(i).size();j++){
 			rapidjson::Value v ;
 			v.SetObject();
-			v.AddMember("playername","2", allocator);
+			rapidjson::Document::AllocatorType& allocator = d.GetAllocator();
+			v.AddMember("","2", allocator);
+			columns.at(0).PushBack(v);
 		}
 	}
 	v.SetObject();
-	rapidjson::Document::AllocatorType& allocator = d.GetAllocator();
+
 	v.AddMember("playername","2", allocator);
 	//d.AddMember("",rapidjson::Value(rows.at(0).at(0)));
 
