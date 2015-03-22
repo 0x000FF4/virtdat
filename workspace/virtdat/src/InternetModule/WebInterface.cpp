7f2dab9ccf3cd3d44efe4011b1a735ce0608b43d
0x000FF4 <ddd32@abv.bg>
2015-03-22 22:55:37 +0200

    web innterface start

---
+++ workspace/virtdat/src/InternetModule/WebInterface.cpp
@@ -0,0 +1,21 @@
+/*
+ * WebInterface.cpp
+ *
+ *  Created on: 22.03.2015 г.
+ *      Author: x000ff4
+ */
+
+#include "WebInterface.h"
+
+namespace virtdat {
+
+WebInterface::WebInterface() {
+	// TODO Auto-generated constructor stub
+
+}
+
+WebInterface::~WebInterface() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace virtdat */
---
+++ workspace/virtdat/src/InternetModule/WebInterface.h
@@ -0,0 +1,21 @@
+/*
+ * WebInterface.h
+ *
+ *  Created on: 22.03.2015 г.
+ *      Author: x000ff4
+ */
+
+#ifndef INTERNETMODULE_WEBINTERFACE_H_
+#define INTERNETMODULE_WEBINTERFACE_H_
+
+namespace virtdat {
+
+class WebInterface {
+public:
+	WebInterface();
+	virtual ~WebInterface();
+};
+
+} /* namespace virtdat */
+
+#endif /* INTERNETMODULE_WEBINTERFACE_H_ */
